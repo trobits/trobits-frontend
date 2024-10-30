7e6689dc120b947c81d105d676f0e9f5a88df856
mottakinrahat <rahatchowdhury661@gmail.com>
2024-10-30 20:39:33 +0600

    Done

--- src/components/Constant/Navbar.constant.ts
+++ src/components/Constant/Navbar.constant.ts
@@ -1,6 +1,6 @@
 export const navItems = [
     { name: 'Home', href: '/' },
-    { name: 'CryptoHub', href: '/cryptohub' },
+    { name: 'CryptoHub', href: '/cr' },
     { name: 'Learn', href: '/learn' },
     { name: 'Basics', href: '/basics' },
     { name: 'Price', href: '/price' },
