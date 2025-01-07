ab12cb3473ebab0f0eb9c00a2493a4c3b83be209
Nasir Mollah <nasirmollah192@gmail.com>
2025-01-07 18:28:36 +0600

    change price page widget and add two menu after crypto info

---
+++ src/components/HomePages/cryptoInfo/CryptoInfo.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
+
+import OtherVideoModal from "@/components/VideoModal/OtherVideoModal";
+import Link from "next/link";
+import React, { useState } from "react";
+
+const CryptoInfo = () => {
+  const [isMOdalOpen, setIsModalOpen] = useState(false);
+  return (
+    <>
+      <div className=" flex container mx-auto mt-20 text-white items-center justify-center">
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="mr-20 font-bold cursor-pointer px-6 py-2 rounded-md bg-purple-600"
+        >
+          Demo Video
+        </button>
+        <Link
+          href={"/howitworks"}
+          className="font-bold cursor-pointer px-6 py-2 rounded-md bg-purple-600"
+        >
+          How Trobits Work
+        </Link>
+      </div>
+      {isMOdalOpen && (
+        <OtherVideoModal
+          isOpen={isMOdalOpen}
+          onClose={() => setIsModalOpen(false)}
+        />
+      )}
+    </>
+  );
+};
+
+export default CryptoInfo;
