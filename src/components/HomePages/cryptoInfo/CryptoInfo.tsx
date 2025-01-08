64736904d40a934c81a6e0778966ad3f7f8adc40
Nasir Mollah <nasirmollah192@gmail.com>
2025-01-08 12:25:40 +0600

    remove email from user profile and fetch all related post of a particular user

--- src/components/HomePages/cryptoInfo/CryptoInfo.tsx
+++ src/components/HomePages/cryptoInfo/CryptoInfo.tsx
@@ -12,13 +12,13 @@ const CryptoInfo = () => {
       <div className=" flex container mx-auto mt-20 text-white items-center justify-center">
         <button
           onClick={() => setIsModalOpen(true)}
-          className="mr-20 font-bold cursor-pointer px-6 py-2 rounded-md bg-purple-600"
+          className="mr-20 font-bold cursor-pointer px-6 py-2 rounded-md bg-cyan-500"
         >
           Demo Video
         </button>
         <Link
           href={"/howitworks"}
-          className="font-bold cursor-pointer px-6 py-2 rounded-md bg-purple-600"
+          className="font-bold cursor-pointer px-6 py-2 rounded-md bg-cyan-500"
         >
           How Trobits Work
         </Link>
