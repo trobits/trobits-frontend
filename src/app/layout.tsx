b3aa616ddfd7036c5943c7d41e149e94e87a3def
trobits <trobits@protonmail.com>
2025-02-08 21:37:43 -0500

    New ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
           }}
         />
 
-        {/* Ad Banner */}
+        {/* Ad Banners */}
         <AdBanner />
 
         {/* App Providers */}
@@ -68,7 +68,7 @@ export default function RootLayout({
   );
 }
 
-// ✅ AdBanner Component (Includes Both Ads)
+// ✅ AdBanner Component (Includes All Ads)
 function AdBanner() {
   return (
     <>
@@ -115,6 +115,30 @@ function AdBanner() {
                 a.onerror=function(){a.remove(),(m+=1)>=r.length||e(n,c,t,o,r,m)},
                 s.parentNode.insertBefore(a,s)
               }(window,document,"script","67a80a6f40c2d4901be49056",["cdn.bmcdn6.com"], 0, new Date().getTime())
+            }();
+          `,
+        }}
+      />
+
+      {/* ✅ New Third Ad Banner (Added as requested) */}
+      <ins
+        className="67a80af19c91f9bf7c42d68b"
+        style={{ display: "inline-block", width: "120px", height: "600px" }}
+      ></ins>
+
+      <Script
+        id="new-ad-banner-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(e,n,c,t,o,r,d){
+              !function e(n,c,t,o,r,m,d,s,a){
+                s=c.getElementsByTagName(t)[0],
+                (a=c.createElement(t)).async=!0,
+                a.src="https://"+r[m]+"/js/"+o+".js?v="+d,
+                a.onerror=function(){a.remove(),(m+=1)>=r.length||e(n,c,t,o,r,m)},
+                s.parentNode.insertBefore(a,s)
+              }(window,document,"script","67a80af19c91f9bf7c42d68b",["cdn.bmcdn6.com"], 0, new Date().getTime())
             }();
           `,
         }}
