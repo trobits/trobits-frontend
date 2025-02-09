2824140f7b14215bbdd8f5be4057f2b19fc3236b
trobits <trobits@protonmail.com>
2025-02-08 21:29:36 -0500

    New ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -88,31 +88,3 @@ function AdBanner() {
 </>
   );
 }
-function AdBanner() {
-  return (
-<>
-<ins
-        className="67a80a6f40c2d4901be49056"
-        style={{ display: 'inline-block', width: '120px', height: '600px' }}
-></ins>
- 
-      <Script
-      id="ad-banner-script-loader"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(e,n,c,t,o,r,d){
-              !function e(n,c,t,o,r,m,d,s,a){
-                s=c.getElementsByTagName(t)[0],
-                (a=c.createElement(t)).async=!0,
-                a.src="https://"+r[m]+"/js/"+o+".js?v="+d,
-                a.onerror=function(){a.remove(),(m+=1)>=r.length||e(n,c,t,o,r,m)},
-                s.parentNode.insertBefore(a,s)
-              }(window,document,"script","67a80a6f40c2d4901be49056",["cdn.bmcdn6.com"], 0, new Date().getTime())
-            }();
-          `,
-      }}
-      />
-</>
-  );
-}
