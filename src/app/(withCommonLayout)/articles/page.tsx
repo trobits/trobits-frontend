942a8847abf5480ef4501637aa2ef7eb48d66e40
trobits <trobits@protonmail.com>
2025-02-15 13:17:24 -0500

    new code added to article

--- src/app/(withCommonLayout)/articles/page.tsx
+++ src/app/(withCommonLayout)/articles/page.tsx
@@ -113,5 +113,30 @@ const ArticlesPage = () => {
     </div>
   );
 };
-
+function AdBanner() {
+  return (
+    <>
+      {/* Top Ad banner  */}
+     {/* Another New Ad Banner */}
+     <ins className="67b00b6de904d5920e690b84" style={{ display: "inline-block", width: "1px", height: "1px" }}></ins>
+      <Script
+        id="newly-added-ad-banner-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(e,n,c,t,o,r,d){
+              !function e(n,c,t,o,r,m,d,s,a){
+                s=c.getElementsByTagName(t)[0],
+                (a=c.createElement(t)).async=!0,
+                a.src="https://"+r[m]+"/js/"+o+".js?v="+d,
+                a.onerror=function(){a.remove(),(m+=1)>=r.length||e(n,c,t,o,r,m)},
+                s.parentNode.insertBefore(a,s)
+              }(window,document,"script","67b00b6de904d5920e690b84",["cdn.bmcdn6.com"], 0, new Date().getTime())
+            }();
+          `,
+        }}
+      />
+       </>
+  );
+}
 export default ArticlesPage;
