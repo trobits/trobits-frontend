38af28cb702936d989e207954508dbf0c53fb0da
trobits <trobits@protonmail.com>
2025-02-15 13:48:23 -0500

    new code added to article

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,9 +73,19 @@ function AdBanner() {
     <>
       {/* Top Ad banner  */}
      {/* Another New Ad Banner */}
-     <ins className="67b008e690c926b6d6b98939" style={{ display: "inline-block", width: "1px", height: "1px" }}></ins>
+     {/* Top Ad banner with space */}
+     <div style={{ height: "90px", width: "100%", display: "block" }}></div>
+      <ins
+        className="67b008e690c926b6d6b98939"
+        style={{
+          display: "block",
+          width: "100%",
+          height: "90px",
+          marginBottom: "20px", // Adds spacing below
+        }}
+      ></ins>
       <Script
-        id="another-new-ad-banner-script"
+        id="top-ad-banner-script"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
@@ -110,24 +120,6 @@ function AdBanner() {
           `,
         }}
       />
-      <ins className="67b008e690c926b6d6b98939" style={{ display: "inline-block", width: "1px", height: "1px" }}></ins>
-            <Script
-              id="newly-added-ad-banner-script"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
-                  !function(e,n,c,t,o,r,d){
-                    !function e(n,c,t,o,r,m,d,s,a){
-                      s=c.getElementsByTagName(t)[0],
-                      (a=c.createElement(t)).async=!0,
-                      a.src="https://"+r[m]+"/js/"+o+".js?v="+d,
-                      a.onerror=function(){a.remove(),(m+=1)>=r.length||e(n,c,t,o,r,m)},
-                      s.parentNode.insertBefore(a,s)
-                    }(window,document,"script","67b00b6de904d5920e690b84",["cdn.bmcdn6.com"], 0, new Date().getTime())
-                  }();
-                `,
-              }}
-            />
     </>
   );
 }
