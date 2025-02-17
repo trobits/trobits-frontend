d8a94c50f8f2625fa423e934e890b0ef901a5ccf
trobits <trobits@protonmail.com>
2025-02-17 17:14:20 -0500

    testing changes on the article page tsx

--- src/app/(withCommonLayout)/articles/page.tsx
+++ src/app/(withCommonLayout)/articles/page.tsx
@@ -83,7 +83,62 @@ function AdBannerB() {
     </>
   );
 }
-
+function AdBannerC() {
+  return (
+    <>
+      {/* Ad banner */}
+      <ins
+        className="67b00b6de904d5920e690b84"
+        style={{ display: "inline-block", width: "1px", height: "1px" }}
+      ></ins>
+      <Script
+        id="ad-banner-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(e,n,c,t,o,r,d){
+              !function e(n,c,t,o,r,m,d,s,a){
+                s=c.getElementsByTagName(t)[0],
+                (a=c.createElement(t)).async=!0,
+                a.src="https://"+r[m]+"/js/"+o+".js?v="+d,
+                a.onerror=function(){a.remove(),(m+=1)>=r.length||e(n,c,t,o,r,m)},
+                s.parentNode.insertBefore(a,s)
+              }(window,document,"script","67b00b6de904d5920e690b84",["cdn.bmcdn6.com"], 0, new Date().getTime())
+            }();
+          `,
+        }}
+      />
+    </>
+  );
+}
+function AdBannerD() {
+  return (
+    <>
+      {/* Ad banner */}
+      <ins
+        className="67b00b6de904d5920e690b84"
+        style={{ display: "inline-block", width: "1px", height: "1px" }}
+      ></ins>
+      <Script
+        id="ad-banner-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(e,n,c,t,o,r,d){
+              !function e(n,c,t,o,r,m,d,s,a){
+                s=c.getElementsByTagName(t)[0],
+                (a=c.createElement(t)).async=!0,
+                a.src="https://"+r[m]+"/js/"+o+".js?v="+d,
+                a.onerror=function(){a.remove(),(m+=1)>=r.length||e(n,c,t,o,r,m)},
+                s.parentNode.insertBefore(a,s)
+              }(window,document,"script","67b00b6de904d5920e690b84",["cdn.bmcdn6.com"], 0, new Date().getTime())
+            }();
+          `,
+        }}
+      />
+    </>
+  );
+}
 
 const ArticlesPage = () => {
   const [ currentPage, setCurrentPage ] = useState(1);
