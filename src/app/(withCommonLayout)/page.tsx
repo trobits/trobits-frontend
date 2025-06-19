802fd5e158e3b10d9900dc8903dcd4bde02f30c2
AffanAnwar <mian.affananwar@gmail.com>
2025-06-19 22:39:01 +0500

    chore: removed debug statements

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -26,21 +26,16 @@ const Profile = () => {
     <div>
       <CryptoNavbar />
 
-      {/* Printrendy above grid */}
       <div className="pt-24">
-        {/* Gemini Card in centered container */}
         <div className="max-w-4xl mx-auto px-4 py-2 mt-32">
           <GeminiCard />
         </div>
 
-        {/* Full-width GridBackground + CryptoData as originally intended */}
         <GridBackground>
           <CryptoData />
         </GridBackground>
       </div>
 
-      {/* Gemini below grid */}
-      {/* Multiple affiliate cards in a single row */}
       <div className="max-w-6xl mx-auto px-4 py-6">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           <GeminiCard />
@@ -50,19 +45,15 @@ const Profile = () => {
 
       <CryptoInfo />
 
-      {/* Ursime after CryptoInfo */}
       <div className="max-w-6xl mx-auto px-4 py-6">
         <UrsimeCard />
       </div>
 
-      {/* Full-width wrapper with inner constrained content */}
       <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 px-4 lg:px-12">
-        {/* SubPage takes 3/4 of space now */}
         <div className="lg:col-span-3">
           <SubPage simpleHeader />
         </div>
 
-        {/* Sidebar takes 1/4 */}
         <div className="space-y-4">
           <GeminiCard />
           <PrintrendyCard />
